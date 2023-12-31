06b4b89ce7cbe5ece93244875532b228d8c1dee4
Alexandre Consigny <alexandre.consigny@gmail.com>
2023-12-31 16:04:46 -0500

    feat: setup vite + ts for pixi

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-const app = new PIXI.Application({ width: 640, height: 360 });
+import * as PIXI from 'pixi.js';
+
+const app = new PIXI.Application<HTMLCanvasElement>({ width: 640, height: 360 });
 
 document.body.appendChild(app.view);
 
