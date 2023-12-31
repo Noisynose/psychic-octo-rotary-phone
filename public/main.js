302b7056cf069d9747ac42669b9406cf82828770
Alexandre Consigny <alexandre.consigny@gmail.com>
2023-12-30 22:12:11 -0500

    feat: add animations

--- public/main.js
+++ public/main.js
@@ -4,3 +4,9 @@ document.body.appendChild(app.view);
 
 const sprite = PIXI.Sprite.from('test-asset.png');
 app.stage.addChild(sprite);
+
+let elapsed = 0.0;
+app.ticker.add((delta) => {
+    elapsed += delta;
+    sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+});
