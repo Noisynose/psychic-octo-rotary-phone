c08cea7b551536c3c0c915b1dc7140efc4ec3ac7
Alexandre Consigny <alexandre.consigny@gmail.com>
2024-01-07 16:03:05 -0500

    chore: clean up view

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,5 @@ const appRoot = document.getElementById('app');
 
 if (appRoot) {
     const app = new PIXI.Application<HTMLCanvasElement>();
-
     document.body.appendChild(app.view);
-    
-    const sprite = PIXI.Sprite.from('test-asset.png');
-    app.stage.addChild(sprite);
-    
-    let elapsed = 0.0;
-    app.ticker.add((delta) => {
-        elapsed += delta;
-        sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
-    });
 }
